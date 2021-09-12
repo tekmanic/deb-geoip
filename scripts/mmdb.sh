a6a0dfc6920f13a9e57381935d040490ebdc5387
tekmanic <manictek@gmail.com>
2021-09-12 00:34:47 -0400

    change interpreter

--- scripts/mmdb.sh
+++ scripts/mmdb.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 mkdir GeoLite2
 wget -c https://download.maxmind.com/app/geoip_download\?edition_id=GeoLite2-City\&license_key=$1\&suffix=tar.gz -O - | tar -C GeoLite2 --strip-components 1 -xz 
 mv GeoLite2/GeoLite2-City.mmdb $2/GeoLite2-City.mmdb
